9442be7da709af7e636dfda4f996dab74c6da86c
neptunia558822 <113116634+neptunia558822@users.noreply.github.com>
2024-03-12 13:58:59 +0700

    ทำสคิปเสร็จ

---
+++ Assets/Script/Attraction.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private float myG = 6.67f;
+
+    public static List<Attraction> planetAttractions;
+
+    void Attract(Attraction other)
+    {
+        Rigidbody rbOther = other.rb;
+
+        Vector3 direction = rb.position - rbOther.position;
+
+        float distance = direction.magnitude;
+
+        float forceMagnitude = myG * (rb.mass * rbOther.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbOther.AddForce(force);
+    }
+
+    private void OnEnable()
+    {
+        if (planetAttractions == null) 
+        {
+            planetAttractions = new List<Attraction>();
+        }
+
+        planetAttractions.Add(this);
+
+    }
+
+    void FixedUpdate()
+    {
+        foreach (var pAttraction in planetAttractions) 
+        {
+            if (pAttraction != this)
+            {
+                Attract(pAttraction);
+            }
+        }
+    }
+}
